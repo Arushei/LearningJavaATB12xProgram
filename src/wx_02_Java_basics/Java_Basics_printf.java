245a3d0ec4e336ac35083aba909d4c8585bad007
Arushei <rshsingh107@gmail.com>
2025-11-11 23:18:31 +0530

    Java_Basics_printf.java

--- src/wx_02_Java_basics/Java_Basics_printf.java
+++ src/wx_02_Java_basics/Java_Basics_printf.java
@@ -4,11 +4,18 @@ public class Java_Basics_printf {
     public static void main(String[] args) {
         int a = 10;
         System.out.println(a);
-        System.out.printf("Your variable is %d", a);
+        System.out.printf("Your variable is %d", a); //printf is use for formatting string
         //%d - int, byte, short, long -data type
         //%s - string
         //%f - float, double
         //%b - bollean
+
+        int b = 20;
+        System.out.printf("a value is %d , b value is %d" ,a,b);
+        System.out.println(); //it will add new line
+        System.out.printf("a value is = % d , b value is = %d",a,b);
+        System.out.println();
+        System.out.printf("a = %d , b = %d", a,b);
 
     }
 }
