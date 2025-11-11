42a803caf0c67ac52a2c22a978573253e5e5ab09
Arushei <rshsingh107@gmail.com>
2025-11-11 23:03:58 +0530

    Java_Basics_printf.java

---
+++ src/ex_01_Java_Basics/Variable_01.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Variable_01 {
+    public static void main(String[] args) {
+        int a = 10;
+            a = 50 ;
+        System.out.println(a);
+    }
+}
---
+++ src/wx_02_Java_basics/Java_Basics_printf.java
@@ -0,0 +1,14 @@
+package wx_02_Java_basics;
+
+public class Java_Basics_printf {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.printf("Your variable is %d", a);
+        //%d - int, byte, short, long -data type
+        //%s - string
+        //%f - float, double
+        //%b - bollean
+
+    }
+}
