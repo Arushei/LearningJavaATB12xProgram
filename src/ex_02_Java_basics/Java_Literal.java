167911110d7b3af74954800d1ca360c1a57e5954
Arushei <rshsingh107@gmail.com>
2025-11-12 01:15:44 +0530

    Java_Basics_printf.java

---
+++ src/ex_02_Java_basics/Java_Literal.java
@@ -0,0 +1,20 @@
+package ex_02_Java_basics;
+
+import java.sql.SQLOutput;
+
+public class Java_Literal {
+    public static void main(String[] args) {
+        // Escape char
+        char new_line = '\n';
+        char tab_line = '\t';
+        char back_space = '\b';
+        char carriage_return = '\r';
+
+        System.out.println("Arushei A Singh");
+        System.out.println("Arushei A \n Singh");
+        System.out.println("Arushei \t A Singh");
+        System.out.println("Arushei A\b Singh");
+
+
+    }
+}
